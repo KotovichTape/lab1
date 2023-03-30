e7ca545718308f2113d3514c0022fe2bc4a21118
KotovichTape <isip_d.d.kotovich@mpt.ru>
2023-03-30 14:28:45 +0300

    Добавьте файлы проекта.

---
+++ lab1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using lab1.masterDataSetTableAdapters;
+
+namespace lab1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HumanTableAdapter Human = new HumanTableAdapter();
+        public MainWindow()
+        {
+            InitializeComponent();
+            HumanDataGrid.ItemsSource = Human.GetData();
+            HumanDataGrid.DisplayMemberPath = "Name";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window = new Window1();
+            window.ShowDialog(); 
+        }
+    }
+}
